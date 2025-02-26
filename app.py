17b59eff909946cf7b5b9fcfe1e675440f904cb2
DanielNolberto <danni.nolberto25@gmail.com>
2025-02-26 13:08:58 -0600

    Update ichangeduser.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from flask_cors import CORS, cross_origin
 con = mysql.connector.connect(
     host="185.232.14.52",
     database="u760464709_23005336_bd",
-    user="u760464709_16005339_usr",
+    user="u760464709_23005336_usr",
     password="4hpY9Z[z"
 )
 
